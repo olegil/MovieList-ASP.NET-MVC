2c329a2c5ae08990baf4fffc5ddad76d23ca2c0d
flemmo87 <flemmo87@gmail.com>
2014-05-07 11:42:36 +0200

    Added Helpers: "Movie" and "Utility"

---
+++ MovieList/Helpers/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieList.Helpers
+{
+    public class Movie
+    {
+        public string Title;
+        public int Year;
+        public string Rated;
+        public DateTime Released;
+        public string Runtime;
+        public string Genre;
+        public string Director;
+        public string Writer;
+        public string Actors;
+        public string Plot;
+        public string Language;
+        public string Awards;
+        public string Poster;
+
+        public Movie()
+        {
+
+        }
+    }
+}
---
+++ MovieList/Helpers/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace MovieList.Helpers
+{
+    public static class Utility
+    {
+        public static Movie GetMovieDetails(string ID)
+        {
+            var json = new WebClient().DownloadString("http://www.omdbapi.com/?i=" + ID);
+            Movie m = JsonConvert.DeserializeObject<Movie>(json);
+
+            return m;
+        }
+    }
+}
