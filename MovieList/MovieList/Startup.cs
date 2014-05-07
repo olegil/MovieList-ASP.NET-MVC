bb8d50a2f81affb5f9b857388bbd60f584f37ad8
flemmo87 <flemmo87@gmail.com>
2014-05-07 11:21:21 +0200

    New Project: Initial Commit

---
+++ MovieList/MovieList/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieList.Startup))]
+namespace MovieList
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
